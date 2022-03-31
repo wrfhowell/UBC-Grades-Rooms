749c7ba5b9ab1335d465be66231becbeca5368bb
howillro <p7w2f@ugrad.cs.ubc.ca>
2022-03-30 23:48:58 -0700

    basic html and js

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,21 @@
-document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+let myArray = [
+]
 
-function handleClickMe() {
-	alert("Button Clicked!");
+buildTable(myArray)
+
+
+
+function buildTable(data){
+	let table = document.getElementById('myTable')
+
+	for (let i = 0; i < data.length; i++){
+		let row = `<tr>
+							<td>${data[i].name}</td>
+							<td>${data[i].age}</td>
+							<td>${data[i].birthdate}</td>
+					  </tr>`
+		table.innerHTML += row
+
+
+	}
 }
